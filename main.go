641b68a1963e04b11816d896b8ff20ab378145c8
mdNazmulHasan <najmul.hasan10@gmail.com>
2019-05-15 11:15:41 +0600

    init commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string= "Ace of spades"
+	// var card = "Ace of spades"
+	card := "ace of spades"
+	card = "five of diamonds"
+	card = 5
+	fmt.Println(card)
+}
+
+func newCard() {
+
+}
