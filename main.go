3469eb6c4ab8efaf080f73dcd124aaee14873339
mdNazmulHasan <najmul.hasan10@gmail.com>
2019-05-15 22:33:28 +0600

    function with return

--- main.go
+++ main.go
@@ -3,14 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// var card string= "Ace of spades"
-	// var card = "Ace of spades"
-	card := "ace of spades"
-	card = "five of diamonds"
-	card = 5
+	card := newCard()
 	fmt.Println(card)
 }
 
-func newCard() {
-
+func newCard() string {
+	return "Five of diamonds"
 }
